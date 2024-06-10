18c04871360592c1e71e45bca3bac1e0323535ac
Huan Phan <pmhuan@tma.com.vn>
2024-06-10 18:56:11 +0700

    refs 2.1.1.認証属性によるログインアクセス制御: Fix IT bugs

--- cas-server-support-osf/src/main/java/io/cos/cas/authentication/handler/support/OpenScienceFrameworkPrincipalFromRequestRemoteUserNonInteractiveCredentialsAction.java
+++ cas-server-support-osf/src/main/java/io/cos/cas/authentication/handler/support/OpenScienceFrameworkPrincipalFromRequestRemoteUserNonInteractiveCredentialsAction.java
@@ -580,14 +580,11 @@ public class OpenScienceFrameworkPrincipalFromRequestRemoteUserNonInteractiveCre
             throw new InstitutionLoginFailedAttributesMissingException("Missing user's names");
         }
 
-        // Call Login Availability API
-        final String sn = user.optString("sn").trim();
+        final String email = user.optString("email").trim();
         final String o = user.optString("o").trim();
         final String ou = user.optString("ou").trim();
-        final String displayName = user.optString("displayName").trim();
         final String eduPersonAffiliation = user.optString("eduPersonAffiliation").trim();
-        final String eduPersonPrincipalName = user.optString("eduPersonPrincipalName").trim();
-        final String eduPersonEntitlement = user.optString("eduPersonEntitlement").trim();
+        final String entitlement = user.optString("entitlement").trim();
         final String eduPersonScopedAffiliation = user.optString("eduPersonScopedAffiliation").trim();
         final String eduPersonTargetedID = user.optString("eduPersonTargetedID").trim();
         final String eduPersonAssurance = user.optString("eduPersonAssurance").trim();
@@ -601,18 +598,18 @@ public class OpenScienceFrameworkPrincipalFromRequestRemoteUserNonInteractiveCre
         final String jaou = user.optString("jaou").trim();
         final String gakuninScopedPersonalUniqueCode = user.optString("gakuninScopedPersonalUniqueCode").trim();
 
-        // send post method to RDM API
+        // Call Login Availability API
         final JSONObject bodyObj = new JSONObject();
         bodyObj.put("institution_id", institutionId);
-        bodyObj.put("mail", username);
-        bodyObj.put("sn", sn);
+        bodyObj.put("mail", email);
+        bodyObj.put("sn", familyName);
         bodyObj.put("o", getStringList(o));
         bodyObj.put("ou", ou);
         bodyObj.put("givenName", givenName);
         bodyObj.put("displayName", fullname);
         bodyObj.put("eduPersonAffiliation", getStringList(eduPersonAffiliation));
-        bodyObj.put("eduPersonPrincipalName", eduPersonPrincipalName);
-        bodyObj.put("eduPersonEntitlement", getStringList(eduPersonEntitlement));
+        bodyObj.put("eduPersonPrincipalName", username);
+        bodyObj.put("eduPersonEntitlement", getStringList(entitlement));
         bodyObj.put("eduPersonScopedAffiliation", getStringList(eduPersonScopedAffiliation));
         bodyObj.put("eduPersonTargetedID", getStringList(eduPersonTargetedID));
         bodyObj.put("eduPersonAssurance", getStringList(eduPersonAssurance));
@@ -626,6 +623,7 @@ public class OpenScienceFrameworkPrincipalFromRequestRemoteUserNonInteractiveCre
         bodyObj.put("jaou", jaou);
         bodyObj.put("gakuninScopedPersonalUniqueCode", getStringList(gakuninScopedPersonalUniqueCode));
 
+        // send post method to RDM API
         HttpResponse httpResponse;
         try {
             httpResponse = callLoginAvailabilityAPI(bodyObj);
@@ -642,7 +640,7 @@ public class OpenScienceFrameworkPrincipalFromRequestRemoteUserNonInteractiveCre
             }
             final JSONObject json = new JSONObject(builder.toString());
             final String loginAvailability = (String) json.get("login_availability");
-            normalizedPayload.put("login_availability", loginAvailability);
+            user.put("login_availability", loginAvailability);
         } catch (final IOException e) {
             logger.error(
                     "[OSF API] Notify Remote Principal Authenticated Failed: Communication Error - {}",
